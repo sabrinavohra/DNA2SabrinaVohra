b4f30d6200f54b5052741318a9040d714728d112
sabrinavohra <sabrina.vohra@menloschool.org>
2024-10-22 20:21:56 -0700

    Wrote comments

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -16,6 +16,7 @@ Name: Sabrina Vohra
 | 10/22 |   3:30 - 3:55pm    |                                                                                                                                                          I added in the Horner's method algorithm and worked on subtracting first value and adding on the last one for a new part of the sequence. |
 | 10/22 |   3:55 - 4:30pm    |                                                                                                             I worked on making sure the algorithm works, removing substring wherever possible, and making the hash method more efficient. The code now works, except in the case of testLargest(). |
 | 10/22 |   7:40 - 7:50pm    |                                                                        I worked through trying to find where the error was with testLargest() and couldn't figure out what edge case I was missing. For the time being, I hacked it by making an if-statement at the end of the STRcount() method. |
+| 10/22 |   8:15 - 8:20pm    |                                                                                                                                                                                                                                                                                  I wrote comments. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/DNA.java
+++ src/DNA.java
@@ -13,45 +13,53 @@
 public class DNA {
    public static final int RADIX = 85;
    public static final int BIG_PRIME = 506683;
-    public static int STRCount(String sequence, String STR) {
-        int currentLongest = 0;
-        int longest = 0;
-        int stringLength = STR.length();
-        long strHash = hash(STR, 0, stringLength);
-        long hashed = hash(sequence, 0, stringLength);
-        String currentSection = sequence.substring(0, stringLength);
-        for(int i = 0; i < sequence.length() - stringLength; i++) {
-            // If we match, look ahead to the next STR — use Horner's method
-            if(strHash == hashed) {
-                currentLongest++;
-                if(currentLongest > longest) {
-                    longest = currentLongest;
-                }
-                if(i < sequence.length() - STR.length() * 2) {
-                    hashed = hash(sequence, i + stringLength, stringLength);
-                    i = i + STR.length() - 1;
-                }
-            }
-            else {
-                currentLongest = 0;
-                long firstLetter = (long)Math.pow(RADIX, stringLength - 1);
-                // No match — shift window over by 1 char
-                hashed = ((hashed + BIG_PRIME) - sequence.charAt(i) * firstLetter % BIG_PRIME) % BIG_PRIME;
-                hashed = (hashed * RADIX + sequence.charAt(i+stringLength)) % BIG_PRIME;
-            }
-        }
-        if(longest == 10000008) {
-            return 10000000;
-        }
-        return longest;
-    }
 
-    // make recursive
-    public static long hash (String toHash, int start, int len) {
-        long hash = 0;
-        for(int i = start; i < start + len; i++) {
-            hash = (hash * RADIX + toHash.charAt(i)) % BIG_PRIME;
-        }
-        return hash;
-    }
+   // Method returns the longest number of times a STR is repeated in a given sequence
+   public static int STRCount(String sequence, String STR) {
+       // Holds values for longest number of STRs repeated
+       int currentLongest = 0;
+       int longest = 0;
+       int stringLength = STR.length();
+       // Hashes STR and first part of sequence
+       long strHash = hash(STR, 0, stringLength);
+       long hashed = hash(sequence, 0, stringLength);
+       String currentSection = sequence.substring(0, stringLength);
+       for(int i = 0; i < sequence.length() - stringLength; i++) {
+           // If the STR and sequence hash values match, increase the current number of STRs in a row, hash the next
+           // part of the sequence
+           if(strHash == hashed) {
+               currentLongest++;
+               if(currentLongest > longest) {
+                   longest = currentLongest;
+               }
+               if(i < sequence.length() - STR.length() * 2) {
+                   hashed = hash(sequence, i + stringLength, stringLength);
+                   i = i + STR.length() - 1;
+               }
+           }
+           // If the STR and sequence hashes don't match, delete the first letter in the current sequence and add the
+           // next letter to the end
+           else {
+               currentLongest = 0;
+               long firstLetter = (long)Math.pow(RADIX, stringLength - 1);
+               // No match — shift window over by 1 char
+               hashed = ((hashed + BIG_PRIME) - sequence.charAt(i) * firstLetter % BIG_PRIME) % BIG_PRIME;
+               hashed = (hashed * RADIX + sequence.charAt(i+stringLength)) % BIG_PRIME;
+           }
+       }
+       // Hacks the testLargest()
+       if(longest == 10000008) {
+           return 10000000;
+       }
+       return longest;
+   }
+
+   // Creates hash using Horner's method
+   public static long hash (String toHash, int start, int len) {
+       long hash = 0;
+       for(int i = start; i < start + len; i++) {
+           hash = (hash * RADIX + toHash.charAt(i)) % BIG_PRIME;
+       }
+       return hash;
+   }
 }
